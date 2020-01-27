5d4ae97c1b2c156c4cc5d09285de268f011b9397
vincentscode <vincentscode@gmail.com>
2020-01-27 21:53:08 +0100

    added nibble

---
+++ actions/nibble.py
@@ -0,0 +1,21 @@
+import discord
+import random
+from helpers import get_gif
+
+commands = ["nibble"]
+requires_mention = True
+accepts_mention = True
+description = "nibble"
+
+
+async def execute(message):
+    if len(message.mentions) == 0:
+        await message.channel.send('Wen denn? o.O')
+        return
+    gif = get_gif(random.choice(['nibble', 'cat nibble']), pos=0, lmt=20, wo_anime=True)
+
+    embed = discord.Embed()
+    msg = '{}, du wurdest {} angeknabbert'.format(message.mentions[0].mention, message.author.mention)
+    embed.description = msg
+    embed.set_image(url=gif)
+    await message.channel.send(embed=embed)
