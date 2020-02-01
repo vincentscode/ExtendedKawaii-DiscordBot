bcb3a3c517c97321df3a9f286bc67c69c917ce91
Vincent Schmandt <vincentscode@gmail.com>
2020-02-01 23:24:15 +0100

    added counter

--- actions/shutup_radio.py
+++ actions/shutup_radio.py
@@ -1,4 +1,5 @@
 import discord
+import shelve
 from helpers import get_gif
 
 commands = ["stfur"]
@@ -11,8 +12,20 @@ async def execute(message):
     embed = discord.Embed()
     if len(message.mentions) != 0:
         msg = 'Shut up, {}!'.format(message.mentions[0].mention)
+        if message.mentions[0].name == "keinkreativerNutzername" and message.mentions[0].discriminator == "5012":
+            shv = shelve.open("shutup_radio.config")
+            if 'lena_counter' in shv:
+                shv['lena_counter'] = shv['lena_counter'] + 1
+                ctr = int(shv.get('lena_counter'))
+            else:
+                shv['lena_counter'] = 1
+                ctr = 1
+            shv.close()
+
+            msg = 'Shut up, {}!\nDu wurdest zum {} Mal mit einem Radio zum schweigen gebracht.'.format(message.mentions[0].mention, ctr)
     else:
         msg = 'Shut up!'
     embed.description = msg
     embed.set_image(url="https://media.tenor.com/images/a3cefe5da142e9ad28dac7d219630696/tenor.gif")
+
     await message.channel.send(embed=embed)
