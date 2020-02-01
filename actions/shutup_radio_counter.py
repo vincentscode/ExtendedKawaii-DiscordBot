c06ca4d2dfb3f81c875ab662072aa2ab5a075b77
Vincent Schmandt <vincentscode@gmail.com>
2020-02-01 23:41:31 +0100

    added counter

---
+++ actions/shutup_radio_counter.py
@@ -0,0 +1,19 @@
+import discord
+import shelve
+
+commands = ["stfurc"]
+requires_mention = False
+accepts_mention = False
+description = "stfurc"
+
+
+async def execute(message):
+    shv = shelve.open("shutup_radio.config")
+    if 'lena_counter' in shv:
+        shv['lena_counter'] = shv['lena_counter'] + 1
+        ctr = int(shv.get('lena_counter'))
+    else:
+        shv['lena_counter'] = 1
+        ctr = 1
+    shv.close()
+    await message.channel.send('Lena wurde schon zum {} Mal mit einem Radio zum schweigen gebracht.'.format(message.mentions[0].mention, ctr))
