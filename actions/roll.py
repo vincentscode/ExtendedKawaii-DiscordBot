770e35034c2db256e81c2d92882de022d42d1594
Vincent Schmandt <vincentscode@gmail.com>
2020-02-02 21:53:10 +0100

    updated roll blacklist

--- actions/roll.py
+++ actions/roll.py
@@ -6,12 +6,14 @@ requires_mention = False
 accepts_mention = True
 description = "Rumrollen owo"
 
+blacklist = ["https://media.tenor.com/images/16b7245d1fa155fb3c20bcaaf022213c/tenor.gif", "https://media.tenor.com/images/f4e0a7df7b2ccecb200521e2a4515f7b/tenor.gif"]
+
 
 async def execute(message):
     params = ('roll', 25, 0, True)
     gif = get_gif(*params)
-    while gif == "https://media.tenor.com/images/16b7245d1fa155fb3c20bcaaf022213c/tenor.gif":
-        print("skipping role gif")
+    while gif in blacklist:
+        print("skipping role gif", blacklist.index(gif))
         gif = get_gif(*params)
 
     embed = discord.Embed()
