643c9aaf6a06c99f10badc7c776aa20fbd862f6d
Vincent Schmandt <vincentscode@gmail.com>
2020-02-27 17:42:05 +0100

    changed lmts

--- actions/hug.py
+++ actions/hug.py
@@ -9,6 +9,7 @@ description = "<:knuddelfin:493889242703986721>"
 
 async def execute(message):
     embed = discord.Embed()
+
     if len(message.mentions) == 1 and message.mentions[0].mention == message.author.mention:
         # self mention
         embed.description = f"{message.author.mention} umarmt sich selbst owo"
@@ -22,7 +23,7 @@ async def execute(message):
     elif len(message.mentions) > 1:
         # > 1 mentions
         embed.description = f"{message.author.mention} umarmt {', '.join([x.mention for x in message.mentions[:-2]]) + ', ' if len(message.mentions[:-2]) > 0 else ''}{' & '.join([x.mention for x in message.mentions[-2:]])} <:knuddelfin:493889242703986721>"
-        gif = get_gif('group hug', lmt=25, pos=0)
+        gif = get_gif('group hug', lmt=15, pos=0)
 
     else:
         # 0 mentions
