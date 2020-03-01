70c162f15db3fd2c42e4a91375cf5396a880fed0
Vincent Schmandt <vincentscode@gmail.com>
2020-03-01 14:56:39 +0100

    updated ship

--- actions/ship.py
+++ actions/ship.py
@@ -1,5 +1,6 @@
 import discord
 from helpers import get_gif
+import random
 
 commands = ["schiff", "couple"]
 requires_mention = True
@@ -15,8 +16,13 @@ async def execute(message: discord.Message):
     name_a = message.guild.get_member(message.mentions[0].id).display_name
     name_b = message.guild.get_member(message.mentions[1].id).display_name
 
+    ship_name = name_a[:int(len(name_a) / 2)] + name_b[int(len(name_b) / 2):]
+    random.seed(message.mentions[0].id+message.mentions[1].id)
+    love_calc = random.randint(0, 100)
+
     e = discord.Embed()
     e.title = ':heart: Lovely Shipping! :heart:'
-    e.description = "Shipping Name: **" + name_a[:int(len(name_a) / 2)] + name_b[int(len(name_b) / 2):] + "**"
+    e.description = f"Shipping Name: **{ship_name}**\n" \
+                    f"Liebe zwischen {name_a} & {name_b}: **{love_calc}%**"
 
     await message.channel.send(embed=e)
