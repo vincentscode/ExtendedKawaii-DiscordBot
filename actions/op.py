ae0a5630d784fd5ffbdca52abfb27c81718ae82a
Vincent Schmandt <vincentscode@gmail.com>
2020-03-05 22:38:40 +0100

    added bot filter

--- actions/op.py
+++ actions/op.py
@@ -15,7 +15,8 @@ async def execute(message):
     e.title = 'Liste der authorisierten User'
 
     authorized_ids = [x for x in config.admin_ids]
-    server_authorized_ids = [member.id for member in message.guild.members if (member.guild_permissions.administrator or member.guild_permissions.manage_messages or member.guild_permissions.manage_roles) and member.id not in authorized_ids]
+    discord.Member()._user.bot
+    server_authorized_ids = [member.id for member in message.guild.members if (member.guild_permissions.administrator or member.guild_permissions.manage_messages or member.guild_permissions.manage_roles) and member.id not in authorized_ids and not member._user.bot]
 
     e.description = f"**Programmierer**\n<@{authorized_ids[0]}>\n\n**Trusted Users**\n"
     for id in authorized_ids[1:]:
