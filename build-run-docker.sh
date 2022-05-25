4cfe6afa07f9602c54f815c9d869be37167f0d6c
Vincent Schmandt <vincentscode@gmail.com>
2022-05-25 12:03:48 +0200

    Update Docker Run Script

--- build-run-docker.sh
+++ build-run-docker.sh
@@ -10,4 +10,14 @@ docker run --rm --name extendedkawaiibot-test -d \
  -v $(pwd)/server_proposed_actions:/usr/src/app/server_proposed_actions \
  -v $(pwd)/server_webhooks:/usr/src/app/server_webhooks \
  -v $(pwd)/messages_per_day_cache:/usr/src/app/messages_per_day_cache \
+ -v $(pwd)/aggressive_config.config.db:/usr/src/app/aggressive_config.config.db \
+ -v $(pwd)/rep_ct_config.config.db:/usr/src/app/rep_ct_config.config.db \
+ -v $(pwd)/rep_last_config.config.db:/usr/src/app/rep_last_config.config.db \
+ -v $(pwd)/sad_config.config.db:/usr/src/app/sad_config.config.db \
+ -v $(pwd)/shutup_radio.config.db:/usr/src/app/shutup_radio.config.db \
+ -v $(pwd)/source_notify.config.db:/usr/src/app/source_notify.config.db \
+ -v $(pwd)/tr_count_user.config.db:/usr/src/app/tr_count_user.config.db \
+ -v $(pwd)/txt_count_user.config.db:/usr/src/app/txt_count_user.config.db \
+ -v $(pwd)/stammbaum.db3:/usr/src/app/stammbaum.db3 \
+ -v $(pwd)/join_leave_log.txt:/usr/src/app/join_leave_log.txt \
  ghcr.io/vincentscode/extendedkawaiibot:latest
