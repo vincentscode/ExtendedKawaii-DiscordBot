8fa5b1be4df83be1f4b27f11ff3eba18d93c4625
Vincent Schmandt <vincentscode@gmail.com>
2024-02-04 16:48:57 +0100

    feat: now with 100% more shark

---
+++ actions/shark.py
@@ -0,0 +1,47 @@
+import discord
+import requests
+from helpers import parse, print
+
+commands = ["shark, hai"]
+requires_mention = False
+accepts_mention = False
+description = "1 shark"
+
+
+class SharkPost:
+    author: str
+    description: str
+    image_url: str
+
+    def __init__(self, author: str, description: str, image_url: str):
+        self.author = author
+        self.description = description
+        self.image_url = image_url
+
+
+async def execute(message):
+    command, channel, params, mentions, author = parse(message)
+    shark = get_shark()
+
+    if shark is None:
+        await message.channel.send("Could not acquire shark :(")
+    
+    e = discord.Embed()
+    e.title = shark.description
+    e.set_image(url=shark.image_url)
+    e.set_footer(text=shark.author)
+    await message.channel.send(embed=e)
+
+
+def get_shark() -> SharkPost:
+    r = requests.get("https://blahaj.transgirl.dev/images/random")
+    if r.status_code != 200:
+        return None
+    
+    j = r.json()
+
+    author = j["author"]
+    description = j["description"]
+    url = j["url"]
+
+    return SharkPost(author, description, url)
